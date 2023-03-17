6ba5a747be85c9dd272398b834fb6bfbb5c9135a
Joseph Gabriel Jacinto <2041504@cognizant.com>
2023-03-17 15:04:08 +0800

    Add project files.

---
+++ RestSharpProject/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RestSharpProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SoapProject/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SoapProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
